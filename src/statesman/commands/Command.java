b54f0125f351b6a873641147446ca96702d9860a
Francis Fajardo <francisdominic.fajardo@gmail.com>
2021-06-08 16:03:12 +0800

    [Command] Remove ID constant from interface

--- src/statesman/commands/Command.java
+++ src/statesman/commands/Command.java
@@ -2,8 +2,6 @@ package statesman.commands;
 
 public interface Command {
 
-    public static final String ID = "";
-
     public abstract Command createInstance(String[] arguments);
 
     public abstract void execute();
